fcc7a198b25292e410d132e9669bbd20cd53ee02
Pavel <pavel@node304.local>
2020-04-11 23:44:24 +0300

    fix indentation & blank lines

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import readlineSync from 'readline-sync';
 
 const userGreeting = () => {
-    const greeting = 'Welcome to the Brain Games!';
-    console.log(greeting);
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
+  const greeting = 'Welcome to the Brain Games!';
+  console.log(greeting);
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 };
 
 export default userGreeting;
-
