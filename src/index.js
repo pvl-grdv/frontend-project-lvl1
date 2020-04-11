53d7ffa7be0bd1ee491770c198c3843fdab7ed69
Pavel <pavel@node304.local>
2020-04-11 22:03:37 +0300

    Added readlineSync and made logic on src/index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import readlineSync from 'readline-sync';
+
+const userGreeting = () => {
+    const greeting = 'Welcome to the Brain Games!';
+    console.log(greeting);
+    const userName = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${userName}!`);
+};
+
+export default userGreeting;
+
