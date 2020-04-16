775c533ceabfe8bc43103af7e24b5163c88727db
Pavel <grdv.pvl@gmail.com>
2020-04-17 02:05:46 +0300

    add new version of brain-even game

---
+++ src/games/brain-even.js
@@ -0,0 +1,22 @@
+import { gameRules, userGreeting, asker, getRandomIntInclusive, answerIsCorrect, answerIsUncorrect, congratulations } from '../index.js';
+
+export default () => {
+  const userName = userGreeting();
+  gameRules('even');
+  let rightAnswersCount = 0;
+  for (let i = 1; i <= 3; i += 1) {
+    const randomNumber = getRandomIntInclusive(0, 1000);
+    const correctAnswer = randomNumber % 2 === 0 ? 'yes' : 'no';
+    const userAnswer = asker(`Question: ${randomNumber}\nYour answer: `);
+    if (userAnswer === correctAnswer) {
+      answerIsCorrect();
+      rightAnswersCount += 1;
+    } else {
+      answerIsUncorrect(userAnswer, correctAnswer, userName);
+      break;
+    }
+  }
+  if (rightAnswersCount === 3) {
+    congratulations(userName);
+  }
+};
