eed57f7f8efcbacc39846d4413f13a9c93d54dfa
Pavel <grdv.pvl@gmail.com>
2020-04-23 22:57:14 +0300

    add separate utils file

---
+++ src/utils.js
@@ -0,0 +1,7 @@
+export const getRandomNumber = (min, max) => {
+  const minCeil = Math.ceil(min);
+  const maxFloor = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloor - minCeil + 1)) + minCeil;
+};
+
+export default getRandomNumber;
