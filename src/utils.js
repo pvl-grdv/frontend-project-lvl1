7631856bb3d83e451c2e7d830ef961089d1ada09
Pavel <grdv.pvl@gmail.com>
2020-04-25 02:37:05 +0300

    simplier randomicer

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,3 @@
-export const getRandomNumber = (min, max) => {
-  const minCeil = Math.ceil(min);
-  const maxFloor = Math.floor(max);
-  return Math.floor(Math.random() * (maxFloor - minCeil + 1)) + minCeil;
-};
+export const getRandomNumber = (min, max) => Math.floor((Math.random() * max) + min);
 
 export default getRandomNumber;
