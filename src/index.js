7b047eaded954cae112793cb96c5b1bb048da84e
Pavel <grdv.pvl@gmail.com>
2020-04-28 01:02:34 +0300

    use destructuring for array read

--- src/index.js
+++ src/index.js
@@ -2,22 +2,18 @@ import readlineSync from 'readline-sync';
 
 export const roundsCount = 3;
 
-// runGame engine
-export const runGame = (questionsAnswers, rule) => {
-  // Greeting
+export const runGame = (rounds, rule) => {
   console.log('Welcome to the Brain Games!');
-  // Ask name
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-  // Show rules
   console.log(rule);
-  // Start game
   for (let i = 0; i < roundsCount; i += 1) {
-    const userAnswer = readlineSync.question(`Question: ${questionsAnswers[i][0]}\nYour answer: `);
-    if (userAnswer === questionsAnswers[i][1]) {
+    const [question, correctAnswer] = rounds[i];
+    const userAnswer = readlineSync.question(`Question: ${question}\nYour answer: `);
+    if (userAnswer === correctAnswer) {
       console.log('Correct!');
     } else {
-      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${questionsAnswers[i][1]}".`);
+      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
       console.log(`Let's try again, ${userName}`);
       return;
     }
