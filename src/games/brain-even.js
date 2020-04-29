0457b6624b1ef64977ed11d6af4fa5cca324187e
Pavel <grdv.pvl@gmail.com>
2020-04-29 16:22:41 +0300

    less consts

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -5,11 +5,9 @@ const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
 const isEven = (number) => number % 2 === 0;
 
 const generateRound = () => {
-  const randomNumber = getRandomNumber(0, 1000);
-  const question = randomNumber;
+  const question = getRandomNumber(0, 1000);
   const correctAnswer = isEven(question) ? 'yes' : 'no';
-  const round = [question, correctAnswer];
-  return round;
+  return [question, correctAnswer];
 };
 
 export default () => {
